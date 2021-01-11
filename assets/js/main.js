89042abea445c048c45f151504087635fd2c55f7
vibhanshujainiiitr <cs19b1027@iiitr.ac.in>
2021-01-11 15:47:34 +0530

    TEDxIITH -> TEDxIIHyderabad

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,3 +21,27 @@ $('.carousel1 .carousel-item').each(function(){
         next.children(':first-child').clone().appendTo($(this));
       }
 });
+
+
+var loader;
+
+function loadNow(opacity) {
+    if (opacity <= 0) {
+        displayContent();
+    } else {
+        loader.style.opacity = opacity;
+        window.setTimeout(function() {
+            loadNow(opacity - 0.05);
+        }, 50);
+    }
+}
+
+function displayContent() {
+    loader.style.display = 'none';
+    document.getElementById('content').style.display = 'block';
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    loader = document.getElementById('loader');
+    loadNow(1);
+});
