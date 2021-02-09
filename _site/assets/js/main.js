2d1674589c32914820aa50c1786f278c6e69eaca
vibhanshujainiiitr <cs19b1027@iiitr.ac.in>
2021-02-09 22:58:38 +0530

    Some intial countdown files added

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -45,3 +45,36 @@ document.addEventListener("DOMContentLoaded", function() {
     loader = document.getElementById('loader');
     loadNow(1);
 });
+
+
+/*========================== Countdown ======================*/
+var countDownDate = new Date("21 March, 2021 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  // document.getElementById("demo").innerHTML = days + "&nbsp;" + hours + "&nbsp;"
+  // + minutes + "&nbsp;" + seconds + "&nbsp;";
+  document.getElementById("day").innerHTML = days;
+  document.getElementById("hour").innerHTML = hours;
+  document.getElementById("minute").innerHTML = minutes;
+   
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "The Event is live !";
+  }
+}, 1000);
