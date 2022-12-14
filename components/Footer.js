899ddcf1685951a26d89996ab9c1df44acee5715
MistyRavager <kushagra.gupta0103@gmail.com>
2022-12-14 11:56:37 +0530

    more accurate footer

--- components/Footer.js
+++ components/Footer.js
@@ -5,49 +5,51 @@ import styles from "./Footer.module.css";
 export default function Footer(){
     return (
         <footer className={`${styles.footer} ${styles.footer_bg_1}`}>
-            <div className={`${styles.circle_ball} d-none d-lg-block`}>
+            {/* <div className={`${styles.circle_ball} d-none d-lg-block`}>
                 <img src="/banner/footer_ball.png" alt=""/>
-            </div>
+            </div> */}
                 <div className={styles.footer_top}>
                     <div className="container">
                         <div className="row">
-                            <div className="col-xl-4 col-md-4 col-lg-4">
+                            <div className={`col-xl-9 col-md-6 col-lg-3 d-none d-lg-block`}>
+                                <img src="/barnd/tedx.png" alt="" height={70} />
+                                <p style={{paddingTop:"30px", color:"white"}}> 
+                                    Something about TEDx IIT Hyderabad.
+                                    Lorem Ipsum Lorem Ipsum Lorem Ipsum 
+                                    Lorem Ipsum Lorem Ipsum Lorem Ipsum 
+                                    Lorem Ipsum Lorem Ipsum Lorem Ipsum 
+                                    orem Ipsum Lorem Ipsum Lorem Ipsum 
+                                    Lorem Ipsum Lorem Ipsum Lorem Ipsum 
+                                    Lorem Ipsum Lorem Ipsum Lorem Ipsum orem Ipsum Lorem Ipsum Lorem Ipsum 
+                                    Lorem Ipsum Lorem Ipsum Lorem Ipsum 
+                                    Lorem Ipsum Lorem Ipsum Lorem Ipsum 
+                                </p>
+                            </div>
+                            <div className="col-xl-2 col-md-2 col-lg-2">
                                 <div className={styles.footer_widget}>
                                     <h3 className={styles.footer_title}>
                                             Follow Us
                                     </h3>
-                                    <ul>
+                                    <ul style={{ listStyle:'none' , padding:0, margin:0}}>
                                         <li><a target="_blank" href="#">Facebook</a></li>
                                         <li><a target="_blank" href="#">Twitter</a></li>
                                         <li><a target="_blank" href="#">Instagram</a></li>
                                         <li><a target="_blank" href="#">Youtube</a></li>
+                                        <li><a target="_blank" href="#">LinkedIn</a></li>
                                     </ul>
         
                                 </div>
                             </div>
-                            <div className="col-xl-4 col-md-4 col-lg-4">
+                            <div className="col-xl-1 col-md-1 col-lg-1">
                                 <div className={styles.footer_widget}>
                                     <h3 className={styles.footer_title}>
                                             Links
                                     </h3>
-                                    <ul>
+                                    <ul style={{ listStyle:'none' , padding:0, margin:0}}>
                                         <li><a target="_blank" href="schedule.html">Schedule</a></li>
                                         <li><a target="_blank" href="speakers.html">Speakers</a></li>
                                         <li><a target="_blank" href="contact.html">Contact</a></li>
                                     </ul>
-                                </div>
-                            </div>
-                            <div className="col-xl-4 col-md-4 col-lg-4">
-                                <div className={styles.footer_widget}>
-                                    <h3 className={styles.footer_title}>
-                                            Venue
-                                    </h3>
-                                    <p>
-                                        200, D-block, Green lane USA <br/>
-                                        edumark@contact.com <br/>
-                                        +10 367 467 8934
-                                        
-                                    </p>
                                 </div>
                             </div>
                         </div>
@@ -59,10 +61,12 @@ export default function Footer(){
                         <div className="row">
                             <div className="col-xl-12">
                                 <p className={`${styles.copy_right} text-center`}>
-    Copyright &copy;<script>document.write(new Date().getFullYear());</script>
-     All rights reserved | This template is made with <i className="fa fa-heart-o" aria-hidden="true"></i>
-      by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-   
+    {/* Copyright &copy; */}
+    {/* <script>document.write(new Date().getFullYear());</script> */} 
+    {/* try and fix above line (hydration error)prolly have to use useEffect and stuff*/ }
+     {/* All rights reserved | This template is made with <i className="fa fa-heart-o" aria-hidden="true"></i>
+      by <a href="https://colorlib.com" target="_blank">Colorlib</a> */}
+   This independent TEDx event is operated under license from TED.
                                 </p>
                             </div>
                         </div>
